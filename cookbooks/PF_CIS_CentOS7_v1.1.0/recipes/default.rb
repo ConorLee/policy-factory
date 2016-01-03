94183c145f5964bd263c92fdc59e49d1c2907401
Conor Lee <ConorLee@incredhippo.com>
2016-01-03 16:22:34 +0000

    Allow for multiple levels of policy.

--- cookbooks/PF_CIS_CentOS7_v1.1.0/recipes/default.rb
+++ cookbooks/PF_CIS_CentOS7_v1.1.0/recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: CIS_CentOS7_v1.1.0
 # Recipe:: default
 #
-# Copyright (C) 2015 YOUR_NAME
+# Copyright (C) 2015 Conor Lee
 #
-# All rights reserved - Do Not Redistribute
+# MIT LICENSE
 #
+
+# Depending upon the level the enviroment variable is given 
+# when calling vagrant up we will do certain things.
+# level 0 will be non-compliant
+# level 1 will be level 1 compliant (scored and demo of unscored)
+# level 2 will be level 2 compliant (scored and demo of unscored)
+case ENV['PF_LEVEL']
+when '0'
+when '1'
+  
+when '2'
+else
+  Chef::Log.warn("Policy level #{ENV['PF_LEVEL']} is not supported at this time.")
+  return
+end
