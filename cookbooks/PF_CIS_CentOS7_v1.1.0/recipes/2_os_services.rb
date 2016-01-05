f9160db351cd2554a8ea1fa5547596f8c64cef68
Conor Lee <ConorLee@incredhippo.com>
2016-01-05 23:10:23 +0000

    Update os services section of CIS CentOS7 v1.1.0 to match ruby comment standards.

--- cookbooks/PF_CIS_CentOS7_v1.1.0/recipes/2_os_services.rb
+++ cookbooks/PF_CIS_CentOS7_v1.1.0/recipes/2_os_services.rb
@@ -1,72 +1,72 @@
-#2.1.1
+# 2.1.1
 package 'telnet-server' do
   action :remove
 end
-#2.1.2
+# 2.1.2
 package 'telnet' do
   action :remove
 end
-#2.1.3
+# 2.1.3
 package 'rsh-server' do
   action :remove
 end
-#2.1.4
+# 2.1.4
 package 'rsh' do
   action :remove
 end
-#2.1.5
+# 2.1.5
 package 'ypbind' do
   action :remove
 end
-#2.1.6
+# 2.1.6
 package 'ypserv' do
   action :remove
 end
-#2.1.7
+# 2.1.7
 package 'tftp' do
   action :remove
 end
-#2.1.8
+# 2.1.8
 package 'tftp-server' do
   action :remove
 end
-#2.1.9
+# 2.1.9
 package 'talk' do
   action :remove
 end
-#2.1.10
+# 2.1.10
 package 'talk-server' do
   action :remove
 end
-#2.1.11
+# 2.1.11
 package 'xinetd' do
   action :remove
 end
-#2.1.12
+# 2.1.12
 service 'chargen-dgram' do
   action [:stop, :disable]
 end
-#2.1.13
+# 2.1.13
 service 'chargen-stream' do
   action [:stop, :disable]
 end
-#2.1.14
+# 2.1.14
 service 'daytime-dgram' do
   action [:stop, :disable]
 end
-#2.1.15
+# 2.1.15
 service 'daytime-stream' do
   action [:stop, :disable]
 end
-#2.1.16
+# 2.1.16
 service 'echo-dgram' do
   action [:stop, :disable]
 end
-#2.1.17
+# 2.1.17
 service 'echo-stream' do
   action [:stop, :disable]
 end
-#2.1.18
+# 2.1.18
 service 'tcpmux-server' do
   action [:stop, :disable]
 end
