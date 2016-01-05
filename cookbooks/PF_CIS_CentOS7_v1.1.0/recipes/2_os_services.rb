b89635384985ca1b5f998d8e1ba196fff9ef7610
Conor Lee <ConorLee@incredhippo.com>
2016-01-05 23:15:58 +0000

    Update os services section of CIS CentOS7 v1.1.0 to include rule titles.

--- cookbooks/PF_CIS_CentOS7_v1.1.0/recipes/2_os_services.rb
+++ cookbooks/PF_CIS_CentOS7_v1.1.0/recipes/2_os_services.rb
@@ -1,72 +1,72 @@
-# 2.1.1
+# 2.1.1 Remove telnet-server (Scored)
 package 'telnet-server' do
   action :remove
 end
-# 2.1.2
+# 2.1.2 Remove telnet Clients (Scored)
 package 'telnet' do
   action :remove
 end
-# 2.1.3
+# 2.1.3 Remove rsh-server (Scored)
 package 'rsh-server' do
   action :remove
 end
-# 2.1.4
+# 2.1.4 Remove rsh (Scored)
 package 'rsh' do
   action :remove
 end
-# 2.1.5
+# 2.1.5 Remove NIS Client (Scored)
 package 'ypbind' do
   action :remove
 end
-# 2.1.6
+# 2.1.6 Remove NIS Server (Scored)
 package 'ypserv' do
   action :remove
 end
-# 2.1.7
+# 2.1.7 Remove tftp (Scored)
 package 'tftp' do
   action :remove
 end
-# 2.1.8
+# 2.1.8 Remove tftp-server (Scored)
 package 'tftp-server' do
   action :remove
 end
-# 2.1.9
+# 2.1.9 Remove talk (Scored)
 package 'talk' do
   action :remove
 end
-# 2.1.10
+# 2.1.10 Remove talk-server (Scored)
 package 'talk-server' do
   action :remove
 end
-# 2.1.11
+# 2.1.11 Remove xinetd (Scored)
 package 'xinetd' do
   action :remove
 end
-# 2.1.12
+# 2.1.12 Disable chargen-dgram (Scored)
 service 'chargen-dgram' do
   action [:stop, :disable]
 end
-# 2.1.13
+# 2.1.13 Disable chargen-stream (Scored)
 service 'chargen-stream' do
   action [:stop, :disable]
 end
-# 2.1.14
+# 2.1.14 Disable daytime-dgram (Scored)
 service 'daytime-dgram' do
   action [:stop, :disable]
 end
-# 2.1.15
+# 2.1.15 Disable daytime-stream (Scored)
 service 'daytime-stream' do
   action [:stop, :disable]
 end
-# 2.1.16
+# 2.1.16 Disable echo-dgram (Scored)
 service 'echo-dgram' do
   action [:stop, :disable]
 end
-# 2.1.17
+# 2.1.17 Disable echo-stream (Scored)
 service 'echo-stream' do
   action [:stop, :disable]
 end
-# 2.1.18
+# 2.1.18 Disable tcpmux-server (Scored)
 service 'tcpmux-server' do
   action [:stop, :disable]
 end
