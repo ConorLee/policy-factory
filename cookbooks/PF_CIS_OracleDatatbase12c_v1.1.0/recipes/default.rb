e58aaf0b57117381895f7d4681eb292b2a8b462d
Conor Lee <ConorLee@incredhippo.com>
2016-01-12 22:55:32 +0000

    Add all recipe links to the default recipe for Oracle Database 12c v1.1.0.

--- cookbooks/PF_CIS_OracleDatatbase12c_v1.1.0/recipes/default.rb
+++ cookbooks/PF_CIS_OracleDatatbase12c_v1.1.0/recipes/default.rb
@@ -2,7 +2,13 @@
 # Cookbook Name:: PF_CIS_OracleDatatbase12c_v1.1.0
 # Recipe:: default
 #
-# Copyright (C) 2016 YOUR_NAME
+# Copyright (C) 2016 Conor Lee
 #
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe "PF_CIS_OracleDatatbase12c_v1.1.0::1_install_patch"
+include_recipe "PF_CIS_OracleDatatbase12c_v1.1.0::1_install_patch_ns"
+include_recipe "PF_CIS_OracleDatatbase12c_v1.1.0::2_parameter_settings"
+include_recipe "PF_CIS_OracleDatatbase12c_v1.1.0::3_connection_login"
+include_recipe "PF_CIS_OracleDatatbase12c_v1.1.0::4_user_access"
+include_recipe "PF_CIS_OracleDatatbase12c_v1.1.0::5_audit_logging"
+
