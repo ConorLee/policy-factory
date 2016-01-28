e050afd44dcae83739920dc883021c383c0a5ac9
Conor Lee <ConorLee@incredhippo.com>
2016-01-28 11:52:41 +0000

    Reorganise the naming system for cookbooks.

---
+++ cookbooks/PF_CIS_CentOS7_v1.1.0_level_2/metadata.rb
@@ -0,0 +1,7 @@
+name             'CIS_CentOS7_v1.1.0_level_2'
+maintainer       'Conor Lee'
+maintainer_email 'ConorLee@outlook.com'
+license          'MIT License'
+description      'Installs/Configures CIS_CentOS7_v1.1.0_level_2'
+long_description 'Installs/Configures CIS_CentOS7_v1.1.0_level_2'
+version          '0.1.0'
